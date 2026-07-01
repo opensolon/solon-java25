7fbc309559ecf5cf40f508faff5973e7cd053ed9
noear <noear@live.cn>
2026-07-01 22:28:59 +0800

    添加 solon ScopeLocalJdk25 自动加载机制

--- src/main/java/org/noear/solon/util/ScopeLocalJdk25.java
+++ src/main/java/org/noear/solon/util/ScopeLocalJdk25.java
@@ -1,17 +1,26 @@
+/*
+ * Copyright 2017-2025 noear.org and authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.noear.solon.util;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.lang.ScopedValue;
 import java.util.function.Supplier;
 
 public class ScopeLocalJdk25<T> implements ScopeLocal<T> {
-    private static final Logger log = LoggerFactory.getLogger(ScopeLocalJdk25.class);
-
     private final ScopedValue<T> ref = ScopedValue.newInstance();
     private final Class<?> applyFor;
-
 
     public ScopeLocalJdk25(Class<?> applyFor) {
         this.applyFor = applyFor;
